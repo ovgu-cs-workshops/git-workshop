966e757e665b381fd4e12acd3ad97ea0e3298d00
Fin Christensen <christensen.fin@gmail.com>
2017-10-30 14:39:20 +0100

    Add tty to each slide for interactive git usage.
    Additionally some styles were fixed to size the terminal correctly.

--- src/app/tty/tty.component.ts
+++ src/app/tty/tty.component.ts
@@ -10,10 +10,15 @@ export class TtyComponent implements OnInit {
   @Input('port') public port: string;
 
   public url: SafeResourceUrl;
+  public loaded: boolean = false;
 
   constructor(private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.url = this._sanitizer.bypassSecurityTrustResourceUrl('//' + location.hostname + ':' + this.port);
+
+    setTimeout(() => {
+      this.loaded = true;
+    }, 1000);
   }
 }
