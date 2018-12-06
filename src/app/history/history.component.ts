b16a9cae1a5999eca4ee83aa3f1fe5e9c463a0dc
Johannes Wünsche <johannes.wuensche97@gmail.com>
2018-12-06 23:17:54 +0100

    Add gitgraph.js compatability

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,272 +1,62 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { DataSet, Network, Node as visNode, Edge as visEdge } from 'vis';
+import { Component, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-interface Node {
-	hash: number;
-  message: string;
-  branch: string;
-  tag: string;
-  shadowed: boolean;
-  color: string;
-  x: number;
-  y: number;
-}
+import 'gitgraph.js';
 
-interface Edge {
-	from: string;
-  to: string;
-  color: string;
-  is_merge: boolean;
-}
+declare var GitGraph: any;
 
-interface Step {
-	nodes: Node[];
-  edges: Edge[];
-}
+let IDENTIFIER = 0;
 
-const node_colors = [
-	"#cddc39",
-  "#ffc107",
-  "#03a9f4",
-  "#8bc34a",
-	"#2196f3",
-  "#673ab7",
-  "#3f51b5",
-  "#00bcd4",
-  "#f44336",
-  "#ff9800",
-  "#009688",
-  "#4caf50",
-  "#9c27b0",
-  "#ff5722",
-  "#795548",
-];
-
-function ParseReflog(reflog: string): Step[] {
-  let idx = 1;
-  const branches = new Map();
-  const colors = new Map();
-	const steps = [];
-
-	for (let line of reflog.split(';')) {
-    line = line.trim();
-
-  	if (line.length <= 0) {
-    	continue;
-    }
-
-  	const re = /(>)?(.*?)(?:->(.*?))?(?::(.*?))?@(.*)/g;
-  	const match = re.exec(line);
-    const new_step = !!match[1];
-    let branch = match[2] || "";
-    const merge = match[3] || "";
-    const tag = match[4] || "";
-    const message = match[5] || "";
-
-  	if (!colors.has(branch)) {
-    	colors.set(branch, node_colors[colors.size]);
-    }
-    const color = colors.get(branch);
-
-    if (!!new_step || steps.length <= 0) {
-    	steps.push({
-        nodes: [],
-        edges: [],
-      });
-
-      // copy branches array to not alter nodes in previous steps
-      for (const [branch_name, that_node] of branches.entries()) {
-      	branches.set(branch_name, {
-        	hash: that_node.hash,
-          message: that_node.message,
-          branch: that_node.branch,
-          tag: that_node.tag,
-          shadowed: that_node.shadowed,
-          color: that_node.color,
-          x: that_node.x,
-          y: that_node.y,
-        });
-      }
-    }
-
-    let old_node = null;
-    if (branches.has(branch)) {
-    	old_node = branches.get(branch);
-    }
-    let current_step = steps[steps.length - 1];
-    let node = {
-    	hash: idx,
-      message: message,
-      branch: branch,
-      tag: tag,
-      shadowed: false,
-      color: color,
-      x: !!old_node ? old_node.x : branches.size,
-      y: idx,
-    };
-    current_step.nodes.push(node);
-
-    let from = idx - 1;
-    let to = idx;
-    if (!!old_node) {
-    	from = old_node.hash;
-      old_node.branch = "";
-
-			if (!current_step.nodes.includes(old_node)) {
-      	current_step.nodes.push(old_node);
-      }
-    }
-    if (from > 0) {
-      current_step.edges.push({
-        from: from,
-        to: to,
-        color: color,
-        is_merge: false,
-      });
-    }
-
-    if (!!merge) {
-      const merge_node = branches.get(merge);
-
-      if (!merge_node) {
-      	console.log("ERROR: Failed to load merge branch " + merge + "...");
-        continue;
-      }
-
-    	node.branch = merge_node.branch;
-      node.color = merge_node.color;
-      node.x = merge_node.x;
-      current_step.edges[current_step.edges.length - 1].is_merge = true;
-      current_step.edges.push({
-      	from: merge_node.hash,
-        to: to,
-        color: merge_node.color,
-        is_merge: false,
-      });
-
-     	merge_node.branch = "";
-      if (!current_step.nodes.includes(merge_node)) {
-      	current_step.nodes.push(merge_node);
-      }
-      old_node.branch = branch;
-      branch = merge;
-    }
-
-    branches.set(branch, node);
-
-    idx += 1;
-  }
-
-  return steps;
-}
 
 @Component({
   selector: 'history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent implements OnInit {
-  @ViewChild('graph') private _graphElement: ElementRef;
-  @ViewChild('spec') private _specElement: ElementRef;
+export class HistoryComponent implements AfterViewInit {
+  @Input() public setup: (config: any) => void;
 
-  private _nodes: DataSet<visNode>;
-  private _edges: DataSet<visEdge>;
-  private _network: Network;
-  private _reflog: Step[];
-  private _current_step: number;
-  private _nodeMap: Map<number, Node>;
-
-  public nodes: Node[];
+	public id: number;
 
   constructor() {
+		this.id = IDENTIFIER++;
   }
 
-  public ngOnInit(): void {
-    this.nodes = [];
-    this._nodeMap = new Map();
-    this._nodes = new DataSet([]);
-    this._edges = new DataSet([]);
-    this._network = new Network(
-      this._graphElement.nativeElement,
-      {
-        nodes: this._nodes,
-        edges: this._edges,
-      },
-      {
-        interaction: {
-          dragNodes: false,
-          dragView: false,
-          hover: false,
-          zoomView: false,
-        },
-        physics: {
-          enabled: false,
-        },
-        edges: {
-          smooth: {
-            enabled: true,
-            type: "horizontal",
-            forceDirection: "none",
-            roundness: 0.5,
+  public ngAfterViewInit(): void {
+		const config = {
+      template: new GitGraph.Template(
+				{
+          colors: ["#979797", "#008fb5", "#f1c109", "#673ab7"],
+          branch: {
+            lineWidth: 10,
+            spacingX: 50,
+            labelRotation: 0
           },
-        },
-        nodes: {
-          font: {
-            size: 24,
-            face: 'Roboto Mono',
-          },
-        },
-      },
-    );
+          commit: {
+            spacingY: -80,
+            dot: {
+              size: 14
+            },
+						tag: {
+							font: 'normal 28pt "Roboto Mono"',
+							color: 'white',
+						},
+            message: {
+              font: "normal 28pt 'Roboto Mono'",
+							displayHash: false,
+							displayAuthor: false,
+            }
+          }
+        }
+			),
+      orientation: 'vertical-reverse',
+      elementId: 'gitGraph-' + this.id,
+			author: '',
+			tagColor: 'white',
+    };
 
-    this._reflog = ParseReflog(this._specElement.nativeElement.innerText);
-    this._current_step = 0;
-    this._renderStep(this._current_step);
-  }
-
-  public nextStep(): void {
-    this._renderStep(++this._current_step);
-  }
-
-  private _renderStep(step_idx: number): void {
-    if (step_idx >= this._reflog.length) {
-      return;
-    }
-
-    const step = this._reflog[step_idx];
-
-    for (const node of step.nodes) {
-      this._nodeMap.set(node.hash, node);
-      this._nodes.update({
-        id: node.hash,
-        label: node.hash.toString(),
-        x: 72 * node.x,
-        y: -72 * node.y,
-        color: node.color,
-      });
-    }
-
-    this.nodes = Array.from(this._nodeMap.values()).reverse();
-
-    for (const edge of step.edges) {
-      this._edges.update({
-        from: edge.from,
-        to: edge.to,
-        width: 3,
-        color: {
-          color: edge.color,
-          highlight: edge.color,
-          hover: edge.color,
-        },
-        smooth: {
-          enabled: true,
-          type: edge.is_merge ? 'vertical' : 'horizontal',
-          roundness: 0.5,
-        },
-      });
-    }
-
-    this._network.fit();
+		if (!!this.setup) {
+    	this.setup(config);
+		}
   }
 }
--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -37,4 +37,148 @@ export class SlidesComponent implements AfterViewInit {
 
     this._changeDetector.detectChanges();
   }
+
+  public agenda(config: any): void {
+    const graph = new GitGraph({
+      ...config,
+      orientation: 'vertical',
+    });
+    const master = graph.branch('master');
+    master.commit('Basics')
+    const basics = master.branch('basics');
+    basics.commit('Introduction to Version Control & Git');
+    basics.commit('Interactive Guide to Git Basics');
+    master.commit('Workflows');
+    const wf = master.branch(<any> {
+      name: 'workflows',
+      column: 1,
+    });
+    wf.commit('Most Common Git Workflows');
+    master.commit('Tricks');
+    const tricks = master.branch(<any> {
+      name: 'tricks',
+      column: 1,
+    });
+    tricks.commit('Advanced Tricks With Git');
+    master.commit('Q&A');
+  }
+
+  public whatIsATag(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('Initial commit');
+    master.commit('Add fancy code');
+    master.commit('Add documentation');
+    master.tag('v0.1.0');
+    master.commit('Fix major bugs');
+  }
+
+  public howAMergeWorks(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('Fix compiler errors');
+    const logging = master.branch('logging');
+    logging.commit('Add logging to application');
+    master.commit('Add translation to german');
+    logging.commit('Add support for log-levels');
+    logging.merge(master);
+  }
+
+  public gitFlowWorkflow(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('Initial commit');
+    const develop = master.branch(<any> {
+      name: 'develop',
+      column: 2
+    });
+    develop.commit('Start development');
+    const feature = develop.branch(<any> {
+      name: 'feature/new-chapter',
+      column: 3
+    });
+    feature.commit('Add new chapter');
+    develop.commit('Fix spelling');
+    feature.merge(develop);
+    const release = develop.branch(<any> {
+      name: 'release-v0.1.0',
+      column: 1
+    });
+    release.commit('Prepare for release');
+    release.merge(master);
+    develop.commit('Start new story');
+    master.tag('v0.1.0');
+  }
+
+  public fix1(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('commit 3');
+    master.commit('commit 4');
+    master.commit('commit 5');
+  }
+
+  public fix2(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2-4');
+    master.commit('commit 5');
+  }
+
+  public reword1(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('commit 3');
+    master.commit('commit 4');
+    master.commit('commit 5');
+  }
+
+  public reword2(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('reword');
+    master.commit('commit 4');
+    master.commit('commit 5');
+  }
+
+  public reword3(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('commit 4');
+    master.commit('commit 5');
+  }
+
+  public cherrypick1(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('commit 3');
+    const dev = master.branch('dev');
+    dev.commit('dev commit 1');
+    dev.commit('dev commit 2');
+    master.commit('commit 4');
+  }
+
+  public cherrypick2(config: any): void {
+    const graph = new GitGraph(config);
+    const master = graph.branch('master');
+    master.commit('commit 1');
+    master.commit('commit 2');
+    master.commit('commit 3');
+    const dev = master.branch('dev');
+    dev.commit('dev commit 1');
+    dev.commit('dev commit 2');
+    master.commit('commit 4');
+    dev.commit('dev commit 4');
+  }
 }
