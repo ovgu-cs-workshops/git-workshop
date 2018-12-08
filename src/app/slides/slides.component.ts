cb273705a5272b43baf729a92e1de96e8b49747f
Willy Failla <willy.failla@travelping.com>
2018-12-08 15:35:21 +0100

    finalizing willy's slides

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -115,6 +115,7 @@ export class SlidesComponent implements AfterViewInit {
   }
 
   public fix1(config: any): void {
+    config.template.commit.widthExtension = 0;
     const graph = new GitGraph(config);
     const master = graph.branch('master');
     master.commit('commit 1');
@@ -165,6 +166,7 @@ export class SlidesComponent implements AfterViewInit {
   }
 
   public cherrypick1(config: any): void {
+    config.template.commit.widthExtension = 0;
     const graph = new GitGraph(config);
     const master = graph.branch('master');
     master.commit('commit 1');
